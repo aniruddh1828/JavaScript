fe1934849aee5225cc3d9e07fb405c44b4f8a622
Ayatha Aniruddh <ayathaaniruddh@Ayathas-MBP.lan>
2024-10-17 11:36:40 -0400

    first commit

---
+++ firstcode.js
@@ -0,0 +1,13 @@
+
+<Html>
+<body>
+    <h1>My First WebPage</h1>
+    <p>My Paragraph</p>
+
+    <p id="demo"></p>
+
+    <script>
+    document.getElementById("demo").innerHTML = 5 + 6;
+    </script>
+</body>
+</Html>
---
+++ scope.js
@@ -0,0 +1,13 @@
+function a(){
+    var b = 10;
+    c();
+    function c(){
+        //console.log(b);
+    }
+    
+}
+
+//var b = 10;
+a();
+
+console.log(b);
